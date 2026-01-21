335294dc38279faee17851b2334bd5466c19a18b
Sabbir Hossain Abed <sabbirhossainabed@Sabbirs-MacBook-Pro.local>
2026-01-21 21:27:07 +0600

    update: admin panel functionalities

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'is_published', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('id', 'title', 'description', 'address', 'city', 'state', 'zip_code', 'price')
+    list_per_page = 25
+    ordering = ('-list_date',)
+
+admin.site.register(Listing, ListingAdmin)
